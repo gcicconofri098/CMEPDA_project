5e3be369af80b2ab6f8d82de67503475bf3dd8ec
GLORIA CICCONOFRI <g.cicconofri@studenti.unipi.it>
2023-12-13 18:26:27 +0100

    tried to fix parameters display for the documentation

--- neutrinos_icecube/hyperparameters.py
+++ neutrinos_icecube/hyperparameters.py
@@ -1,12 +1,11 @@
 '''File that contains the chosen parameters for the Neural Network.
 
     Args:
-
-        :param int n_neighbors: number of neighbors for the knn_graph.
-        :param int N_features: dimensionality of the hidden layers.
-        :param float dropout_value: fraction of dropped connections for the dropout layer.
-        :param int patience: number of epochs to wait before stopping the training.
-        :param float min_delta: value of the validation loss difference between two epochs.
+        n_neighbors (int): number of neighbors for the knn_graph
+        N_features (int): dimensionality of the hidden layers
+        dropout_value (float): fraction of dropped connections for the dropout layer
+        patience (int): number of epochs to wait before stopping the training
+        min_delta (float): value of the validation loss difference between two epochs
 '''
 
 n_neighbors = 7
